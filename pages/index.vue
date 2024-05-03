eb4879fe63dc435f1f8deebb71f7db31bf8b39d7
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-05-03 11:33:22 +0200

    customize the tailwind configs

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-    <div class="text-gray-800 text-3xl underline">welcome to nuxt</div>
+    <div class="text-3xl underline">welcome to nuxt</div>
 </template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./components/**/*.{js,vue,ts}",
-      "./layouts/**/*.vue",
-      "./pages/**/*.vue",
-      "./plugins/**/*.{js,ts}",
-      "./app.vue",
-      "./error.vue",
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./app.vue",
+        "./error.vue",
     ],
     theme: {
-      extend: {},
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+        },
+        fontFamily: {
+            sans:   ['Roboto', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        extend: {
+            colors: {
+                primary: 'var(--color-primary)',
+                secondary: 'var(--color-secondary)',
+                body: 'var(--color-body)',
+                bg: 'var(--color-bg)',
+            },
+        },
     },
     plugins: [],
-  }
+}
