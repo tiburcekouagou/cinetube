094cc591be2cf5f607da1b71cf5447e87ada0a41
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-05-03 11:12:25 +0200

    added tailwindcss

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="">
+        <header>
+            Header
+        </header>
+        <main class="">
+            <slot />
+        </main>
+        <footer class="">
+            Footer
+        </footer>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="text-gray-800 text-3xl underline">welcome to nuxt</div>
+</template>
